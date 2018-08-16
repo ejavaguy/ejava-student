105561c5d697dc8f4fbace9567288c27b088c596
jim <ejavaguy@gmail.com>
2018-08-16 15:30:46 -0400

    updated references to hibernate provider

--- jpa/jpa-utils/jpa-schemagen/src/main/java/ejava/examples/schemagen/JPASchemaGen.java
+++ jpa/jpa-utils/jpa-schemagen/src/main/java/ejava/examples/schemagen/JPASchemaGen.java
@@ -6,7 +6,8 @@ public class JPASchemaGen {
 	public static void main(String args[]) {
 		if (args!=null && args.length==1) {
 			System.out.println("Generating schema for persistence-unit: " + args[0]);
-			Persistence.generateSchema(args[0], null);
+			//Persistence.generateSchema(args[0], null);
+			Persistence.createEntityManagerFactory(args[0]);
 			System.exit(0);
 		} else {
 			System.err.println("usage: " + JPASchemaGen.class.getName() + " (persistenceUnit name)");
